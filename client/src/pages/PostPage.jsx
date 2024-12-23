2a9e74a65411166567107233912fdcdf89779fd4
Richard <richardzou25@gmail.com>
2024-12-23 14:51:11 -0500

    add comment section to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,9 @@
 import { Button, Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CommentSection from "../components/CommentSection";
+import parse from "html-react-parser";
+import DOMPurify from "dompurify";
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -39,34 +42,44 @@ export default function PostPage() {
         <Spinner size="xl" />
       </div>
     );
-  return (
+
+  const sanitizedContent = DOMPurify.sanitize(post.content || "");
+  const parsedContent = parse(sanitizedContent);
+
+  return post ? (
     <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
-      <h1 className="text-2xl mt-10 p3 text-center font=serif max-w-2xl mx-auto lg:text-4xl">
-        {post && post.title}
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+        {post.title}
       </h1>
       <Link
-        to={`/search?category=${post && post.category}`}
-        className="self-center mt-s"
+        to={`/search?category=${post.category}`}
+        className="self-center mt-5"
       >
         <Button color="gray" pill size="xs">
-          {post && post.category}
+          {post.category}
         </Button>
       </Link>
       <img
-        src={post && post.image}
-        alt={post && post.title}
-        className="mt-10 p-3 m-h-[600px] w-full object-cover"
+        src={post.image}
+        alt={post.title}
+        className="mt-10 p-3 max-h-[600px] w-full object-cover"
       />
       <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
-        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span>{new Date(post.createdAt).toLocaleDateString()}</span>
         <span className="italic">
-          {post && (post.content.length / 1000).toFixed(0)} mins read
+          {(post.content.length / 1000).toFixed(0)} mins read
         </span>
       </div>
-      <div
-        className="p-3 max-w-2xl mx-auto w-full post-content"
-        dangerouslySetInnerHTML={{ __html: post && post.content }}
-      ></div>
+      <div className="p-3 max-w-2xl mx-auto w-full post-content">
+        {parsedContent}
+      </div>
+      <div>
+        <CommentSection postId={post._id} />
+      </div>
     </main>
+  ) : (
+    <div className="min-h-screen flex justify-center items-center">
+      No Post Found.
+    </div>
   );
 }
