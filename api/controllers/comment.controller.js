771c2a095459f09cd2f917759fb435275e9b607d
Richard <richardzou25@gmail.com>
2025-01-21 10:33:14 -0500

    add comments section to admin dashboard

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,6 +1,7 @@
 import Comment from "../models/comment.model.js";
 import { errorHandler } from "../utils/error.js";
 
+const MAX_CONTENT_LIMIT = 9;
 export const createComment = async (req, res, next) => {
   try {
     const { content, postId, userId } = req.body;
@@ -90,6 +91,33 @@ export const deleteComment = async (req, res, next) => {
     }
     await Comment.findByIdAndDelete(req.params.commentId);
     res.status(200).json("Comment has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getcomments = async (req, res, next) => {
+  if (!req.user.isAdmin)
+    return next(errorHandler(403, "You are not allowed to get all comments"));
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || MAX_CONTENT_LIMIT;
+    const sortDirection = req.query.sort === "desc" ? -1 : 1;
+    const comments = await Comment.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const totalComments = await Comment.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthComments = await Comment.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({ comments, totalComments, lastMonthComments });
   } catch (error) {
     next(error);
   }
