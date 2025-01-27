26e85202b04a3e9c5e45ca739c4929192618a546
Richard <richardzou25@gmail.com>
2025-01-27 16:30:39 -0500

    Fixed CommonJs and ESM overlap

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
+import tailwindScrollbar from "tailwind-scrollbar";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    ...flowbite.content(),
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    flowbite.plugin(),
-    require("tailwind-scrollbar"),
-    require("@tailwindcss/line-clamp"),
-  ],
+  plugins: [flowbite.plugin(), tailwindScrollbar],
 };
